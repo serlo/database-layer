a8868949159dbd36df95fff3ba44924158d05efc
Jonas Keinholz <git@inyono.dev>
2021-01-11 14:41:37 +0100

    parse DATABASE_URL, Dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{get, web, App, HttpServer, Result};
 use dotenv::dotenv;
+use regex::Regex;
 use sqlx::mysql::{MySqlConnectOptions, MySqlPoolOptions};
+use std::env;
 
 mod uuid;
 
@@ -13,20 +15,34 @@ async fn hello(web::Path(uuid): web::Path<u32>) -> Result<String> {
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
-    // TODO: we can't use DATABASE_URL correctly because we need to override charset.
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+    let re = Regex::new(
+        r"^mysql://(?P<username>.+):(?P<password>.+)@(?P<host>.+):(?P<port>\d+)/(?P<database>.+)$",
+    )
+    .unwrap();
+    let captures = re.captures(&database_url).unwrap();
+    let username = captures.name("username").unwrap().as_str();
+    let password = captures.name("password").unwrap().as_str();
+    let host = captures.name("host").unwrap().as_str();
+    let port: u16 = captures.name("port").unwrap().as_str().parse().unwrap();
+    let database = captures.name("database").unwrap().as_str();
+
     let options = MySqlConnectOptions::new()
-        .host("localhost")
-        .username("root")
-        .password("secret")
-        .database("serlo")
+        .host(host)
+        .port(port)
+        .username(username)
+        .password(password)
+        .database(database)
         .charset("latin1");
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
         .connect_with(options)
         .await?;
 
+    println!("🚀 Server ready: http://localhost:8080");
+
     HttpServer::new(move || App::new().data(pool.clone()).configure(uuid::init))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await?;
 

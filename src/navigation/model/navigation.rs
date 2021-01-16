3eb3fb010f93dc138c64b2d31f5f4c6806bedfc3
Jonas Keinholz <git@inyono.dev>
2021-01-16 11:32:17 +0100

    navigation: handle first level

--- src/navigation/model/navigation.rs
+++ src/navigation/model/navigation.rs
@@ -13,8 +13,30 @@ impl Navigation {
         instance: &str,
         pool: &MySqlPool,
     ) -> Result<Navigation> {
+        let pages = sqlx::query!(
+            r#"
+                SELECT p.id
+                    FROM navigation_page p
+                    JOIN navigation_container c ON c.id = p.container_id
+                    JOIN instance i ON i.id = c.instance_id
+                    JOIN type t ON t.id = c.type_id
+                    WHERE i.subdomain = ? AND t.name = 'default' AND p.parent_id IS NULL
+                    ORDER BY p.position
+            "#,
+            instance
+        )
+        .fetch_all(pool)
+        .await?;
+
+        let mut data = Vec::with_capacity(pages.len());
+
+        for page in pages.iter() {
+            let child = NavigationChild::find_by_id(page.id, pool).await?;
+            data.push(child)
+        }
+
         Ok(Navigation {
-            data: vec![],
+            data,
             instance: String::from(instance),
         })
     }
@@ -25,4 +47,10 @@ pub struct NavigationChild {
     pub id: i32,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub children: Option<Vec<NavigationChild>>,
+}
+
+impl NavigationChild {
+    pub async fn find_by_id(id: i32, pool: &MySqlPool) -> Result<NavigationChild> {
+        Ok(NavigationChild { id, children: None })
+    }
 }

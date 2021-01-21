3dcfa6e8ca5d955d4484a7f317778bb3ba912224
Jonas Keinholz <git@inyono.dev>
2021-01-21 13:17:49 +0100

    license: improve error handling

--- src/license/model.rs
+++ src/license/model.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
 use serde::Serialize;
 use sqlx::MySqlPool;
+use thiserror::Error;
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -15,29 +15,40 @@ pub struct License {
     pub icon_href: String,
 }
 
+#[derive(Error, Debug)]
+pub enum LicenseError {
+    #[error("License cannot be fetched because of a database error: {inner:?}.")]
+    DatabaseError { inner: sqlx::Error },
+    #[error("License cannot be fetched because it does not exist.")]
+    NotFound,
+}
+
 impl License {
-    pub async fn fetch(id: i32, pool: &MySqlPool) -> Result<License> {
-        let license_fut = sqlx::query!(
-            "
+    pub async fn fetch(id: i32, pool: &MySqlPool) -> Result<License, LicenseError> {
+        sqlx::query!(
+            r#"
                 SELECT l.default, l.title, l.url, l.content, l.agreement, l.icon_href, i.subdomain
                     FROM license l
                     JOIN instance i ON i.id = l.instance_id
                     WHERE l.id = ?
-            ",
+            "#,
             id
         )
         .fetch_one(pool)
-        .await?;
-
-        Ok(License {
+        .await
+        .map_err(|error| match error {
+            sqlx::Error::RowNotFound => LicenseError::NotFound,
+            inner => LicenseError::DatabaseError { inner },
+        })
+        .map(|license| License {
             id,
-            instance: license_fut.subdomain,
-            default: license_fut.default == Some(1),
-            title: license_fut.title,
-            url: license_fut.url,
-            content: license_fut.content.unwrap_or_else(|| "".to_string()),
-            agreement: license_fut.agreement,
-            icon_href: license_fut.icon_href.unwrap_or_else(|| "".to_string()),
+            instance: license.subdomain,
+            default: license.default == Some(1),
+            title: license.title,
+            url: license.url,
+            content: license.content.unwrap_or_else(|| "".to_string()),
+            agreement: license.agreement,
+            icon_href: license.icon_href.unwrap_or_else(|| "".to_string()),
         })
     }
 }
--- src/license/routes.rs
+++ src/license/routes.rs
@@ -1,19 +1,23 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use sqlx::MySqlPool;
 
-use super::model::License;
+use super::model::{License, LicenseError};
 
 #[get("/license/{id}")]
 async fn license(id: web::Path<i32>, db_pool: web::Data<MySqlPool>) -> impl Responder {
     let id = id.into_inner();
-    let result = License::fetch(id, db_pool.get_ref()).await;
-    match result {
+    match License::fetch(id, db_pool.get_ref()).await {
         Ok(data) => HttpResponse::Ok()
             .content_type("application/json; charset=utf-8")
             .json(data),
         Err(e) => {
-            println!("Could not get license {}: {:?}", id, e);
-            HttpResponse::BadRequest().json(None::<String>)
+            println!("/license/{:?}: {:?}", id, e);
+            match e {
+                LicenseError::DatabaseError { .. } => {
+                    HttpResponse::InternalServerError().json(None::<String>)
+                }
+                LicenseError::NotFound => HttpResponse::NotFound().json(None::<String>),
+            }
         }
     }
 }

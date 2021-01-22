caeb5a4d63351f5e068db135fd904a8a3732ad7e
Jonas Keinholz <git@inyono.dev>
2021-01-22 02:30:12 +0100

    subscriptions: improve error handling

--- src/subscriptions/model.rs
+++ src/subscriptions/model.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
 use serde::Serialize;
 use sqlx::MySqlPool;
+use thiserror::Error;
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -9,21 +9,31 @@ pub struct Subscriptions {
     pub subscriptions: Vec<Subscription>,
 }
 
+#[derive(Error, Debug)]
+pub enum SubscriptionsError {
+    #[error("Navigation cannot be fetched because of a database error: {inner:?}.")]
+    DatabaseError { inner: sqlx::Error },
+}
+
 #[derive(Serialize)]
 pub struct Subscription {
     pub id: i32,
 }
 
 impl Subscriptions {
-    pub async fn fetch(user_id: i32, pool: &MySqlPool) -> Result<Subscriptions> {
-        let subscriptions_fut = sqlx::query!(
-            "SELECT uuid_id FROM subscription WHERE user_id = ?",
+    pub async fn fetch(
+        user_id: i32,
+        pool: &MySqlPool,
+    ) -> Result<Subscriptions, SubscriptionsError> {
+        let subscriptions = sqlx::query!(
+            r#"SELECT uuid_id FROM subscription WHERE user_id = ?"#,
             user_id
         )
         .fetch_all(pool)
-        .await?;
+        .await
+        .map_err(|inner| SubscriptionsError::DatabaseError { inner })?;
 
-        let subscriptions: Vec<Subscription> = subscriptions_fut
+        let subscriptions: Vec<Subscription> = subscriptions
             .iter()
             .map(|child| Subscription {
                 id: child.uuid_id as i32,

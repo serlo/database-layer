f425057ac700cb833207e9feae64cc9b1265e199
Jonas Keinholz <git@inyono.dev>
2021-01-25 14:23:19 +0100

    subscription: add Subscriptions::fetch_by_user

--- src/subscriptions/routes.rs
+++ src/subscriptions/routes.rs
@@ -1,12 +1,12 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use sqlx::MySqlPool;
 
-use super::model::{Subscriptions, SubscriptionsError};
+use super::model::{SubscriptionsByUser, SubscriptionsError};
 
 #[get("/subscriptions/{user_id}")]
 async fn subscriptions(user_id: web::Path<i32>, db_pool: web::Data<MySqlPool>) -> impl Responder {
     let user_id = user_id.into_inner();
-    match Subscriptions::fetch(user_id, db_pool.get_ref()).await {
+    match SubscriptionsByUser::fetch(user_id, db_pool.get_ref()).await {
         Ok(data) => HttpResponse::Ok()
             .content_type("application/json; charset=utf-8")
             .json(data),

acfe461ca04a50eebae6c2ec69b3766399c88582
Jonas Keinholz <git@inyono.dev>
2021-01-31 20:46:48 +0100

    uuid(page): impl UuidFetcher

--- src/uuid/model/page.rs
+++ src/uuid/model/page.rs
@@ -1,7 +1,10 @@
+use async_trait::async_trait;
+use futures::join;
 use serde::Serialize;
 use sqlx::MySqlPool;
 
-use super::{ConcreteUuid, Uuid, UuidError};
+use super::{ConcreteUuid, Uuid, UuidError, UuidFetcher};
+use crate::database::Executor;
 use crate::datetime::DateTime;
 use crate::format_alias;
 use crate::instance::Instance;
@@ -18,9 +21,9 @@ pub struct Page {
     pub license_id: i32,
 }
 
-impl Page {
-    pub async fn fetch(id: i32, pool: &MySqlPool) -> Result<Uuid, UuidError> {
-        let page = sqlx::query!(
+macro_rules! fetch_one_page {
+    ($id: expr, $executor: expr) => {
+        sqlx::query!(
             r#"
                 SELECT u.trashed, i.subdomain, p.current_revision_id, p.license_id, r.title
                     FROM page_repository p
@@ -29,30 +32,38 @@ impl Page {
                     LEFT JOIN page_revision r ON r.id = p.current_revision_id
                     WHERE p.id = ?
             "#,
-            id
+            $id
         )
-        .fetch_one(pool)
-        .await
-        .map_err(|error| match error {
+        .fetch_one($executor)
+    };
+}
+
+macro_rules! fetch_all_revisions {
+    ($id: expr, $executor: expr) => {
+        sqlx::query!(
+            r#"SELECT id, date FROM page_revision WHERE page_repository_id = ?"#,
+            $id
+        )
+        .fetch_all($executor)
+    };
+}
+
+macro_rules! to_page {
+    ($id: expr, $page: expr, $revisions: expr) => {{
+        let page = $page.map_err(|error| match error {
             sqlx::Error::RowNotFound => UuidError::NotFound,
             error => error.into(),
         })?;
-
-        let revisions = sqlx::query!(
-            r#"SELECT id, date FROM page_revision WHERE page_repository_id = ?"#,
-            id
-        )
-        .fetch_all(pool)
-        .await?;
+        let revisions = $revisions?;
 
         if revisions.is_empty() {
             Err(UuidError::NotFound)
         } else {
             Ok(Uuid {
-                id,
+                id: $id,
                 trashed: page.trashed != 0,
                 // TODO:
-                alias: format_alias(None, id, page.title.as_deref()),
+                alias: format_alias(None, $id, page.title.as_deref()),
                 concrete_uuid: ConcreteUuid::Page(Page {
                     __typename: "Page".to_string(),
                     instance: page
@@ -70,5 +81,33 @@ impl Page {
                 }),
             })
         }
+    }};
+}
+
+#[async_trait]
+impl UuidFetcher for Page {
+    async fn fetch(id: i32, pool: &MySqlPool) -> Result<Uuid, UuidError> {
+        let page = fetch_one_page!(id, pool);
+        let revisions = fetch_all_revisions!(id, pool);
+
+        let (page, revisions) = join!(page, revisions);
+
+        to_page!(id, page, revisions)
+    }
+
+    async fn fetch_via_transaction<'a, E>(id: i32, executor: E) -> Result<Uuid, UuidError>
+    where
+        E: Executor<'a>,
+    {
+        let mut transaction = executor.begin().await?;
+
+        let page = fetch_one_page!(id, &mut transaction).await;
+        let revisions = fetch_all_revisions!(id, &mut transaction).await;
+
+        let result = to_page!(id, page, revisions);
+
+        transaction.commit().await?;
+
+        result
     }
 }

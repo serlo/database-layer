2784c22ddeba48711217ecbdd2dbecb7e90e4522
Jonas Keinholz <git@inyono.dev>
2021-02-06 07:56:23 +0100

    remove `yarn test`

--- __fixtures__/instance.js
+++
@@ -1,7 +0,0 @@
-module.exports = function getInstanceFixtures() {
-  if (process.env.DATABASE_ENV === 'staging') {
-    return ['de', 'en', 'es', 'fr', 'hi', 'ta']
-  }
-
-  return ['de', 'en']
-}
--- __fixtures__/license.js
+++
@@ -1,8 +0,0 @@
-module.exports = function getLicenseFixtures() {
-  if (process.env.DATABASE_ENV === 'staging') {
-    // SELECT max(id) as lastId FROM license
-    return 19
-  }
-
-  return 9
-}

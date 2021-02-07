4fdb3b0a4277fffbfcf509f2cc4e7bc5fe2c6593
Jonas Keinholz <git@inyono.dev>
2021-02-08 00:46:59 +0100

    pacts: wrap requests in transaction
    Close #60

--- src/routes.rs
+++ src/routes.rs
@@ -1,22 +1,43 @@
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpRequest, HttpResponse};
 use sqlx::MySqlPool;
 
 use crate::database::Connection;
 use crate::message::{Message, MessageResponder};
 
 #[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok()
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 #[post("/")]
 async fn handle_message(
+    req: HttpRequest,
     payload: web::Json<Message>,
     db_pool: web::Data<MySqlPool>,
-) -> impl Responder {
+) -> HttpResponse {
+    let rollback = req
+        .headers()
+        .get("Rollback")
+        .map_or(false, |value| match value.to_str() {
+            Ok("true") => true,
+            _ => false,
+        });
     let message = payload.into_inner();
-    let connection = Connection::Pool(db_pool.get_ref());
-    message.handle(connection).await
+    let pool = db_pool.get_ref();
+
+    if rollback {
+        let mut transaction = pool.begin().await.expect("Failed to begin transaction.");
+        let connection = Connection::Transaction(&mut transaction);
+        let response = message.handle(connection).await;
+        transaction
+            .rollback()
+            .await
+            .expect("Failed to roll back transaction.");
+        response
+    } else {
+        let connection = Connection::Pool(pool);
+        message.handle(connection).await
+    }
 }
 
 pub fn init(cfg: &mut web::ServiceConfig) {

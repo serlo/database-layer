034847c5f89f5e91e34d56dedcc7c604e3a070a5
Jonas Keinholz <git@inyono.dev>
2021-02-07 23:48:51 +0100

    routes: use MessageResponderNew

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,8 @@
 use actix_web::{get, post, web, HttpResponse, Responder};
 use sqlx::MySqlPool;
 
-use crate::message::{Message, MessageResponder};
+use crate::database::Connection;
+use crate::message::{Message, MessageResponderNew};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -13,8 +14,10 @@ async fn handle_message(
     payload: web::Json<Message>,
     db_pool: web::Data<MySqlPool>,
 ) -> impl Responder {
+    let pool = db_pool.get_ref();
+    let connection = Connection::Pool(pool);
     let message = payload.into_inner();
-    message.handle(db_pool.get_ref()).await
+    message.handle_new(connection).await
 }
 
 pub fn init(cfg: &mut web::ServiceConfig) {

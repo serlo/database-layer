5b9baea99f850f7bce0cf9a4ad0eaeaf002960b0
Jonas Keinholz <git@inyono.dev>
2021-02-23 15:56:08 +0100

    add sentry

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
+use std::env;
+
 use actix_web::{App, HttpServer, Result};
 
 use serlo_org_database_layer::{configure_app, create_database_pool, ApplicationError};
 
 #[actix_web::main]
 async fn main() -> Result<(), ApplicationError> {
+    let sentry_dsn = env::var("SENTRY_DSN").ok();
+    let _guard = sentry::init((
+        sentry_dsn,
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+    env::set_var("RUST_BACKTRACE", "1");
+
     let pool = create_database_pool().await?;
 
     println!("🚀 Server ready: http://localhost:8080");
 
-    HttpServer::new(move || configure_app(App::new(), pool.clone()))
-        .bind("0.0.0.0:8080")
-        .map_err(|inner| ApplicationError::ServerError { inner })?
-        .run()
-        .await
-        .map_err(|inner| ApplicationError::ServerError { inner })
+    HttpServer::new(move || {
+        let app = App::new().wrap(sentry_actix::Sentry::new());
+        configure_app(app, pool.clone())
+    })
+    .bind("0.0.0.0:8080")
+    .map_err(|inner| ApplicationError::ServerError { inner })?
+    .run()
+    .await
+    .map_err(|inner| ApplicationError::ServerError { inner })
 }

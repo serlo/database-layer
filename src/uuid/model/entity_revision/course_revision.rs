d8d6773419a96e1096f666688879445d4355240e
Botho <1258870+elbotho@users.noreply.github.com>
2021-05-06 12:28:14 +0200

    revert change to interface

--- src/uuid/model/entity_revision/course_revision.rs
+++ src/uuid/model/entity_revision/course_revision.rs
@@ -6,21 +6,21 @@ use super::abstract_entity_revision::AbstractEntityRevision;
 #[serde(rename_all = "camelCase")]
 pub struct CourseRevision {
     title: String,
-    description: String,
+    content: String,
     meta_description: String,
 }
 
 impl From<&AbstractEntityRevision> for CourseRevision {
     fn from(abstract_entity_revision: &AbstractEntityRevision) -> Self {
         let title = abstract_entity_revision.fields.get_or("title", "");
-        let description = abstract_entity_revision.fields.get_or("description", "");
+        let content = abstract_entity_revision.fields.get_or("description", "");
         let meta_description = abstract_entity_revision
             .fields
             .get_or("meta_description", "");
 
         CourseRevision {
             title,
-            description,
+            content,
             meta_description,
         }
     }

d1d4c95402f77b6dcde650c108ba3fb8d647afea
Botho <1258870+elbotho@users.noreply.github.com>
2021-05-06 12:20:18 +0200

    course_revision: replace content w/ description

--- src/uuid/model/entity_revision/course_revision.rs
+++ src/uuid/model/entity_revision/course_revision.rs
@@ -6,21 +6,21 @@ use super::abstract_entity_revision::AbstractEntityRevision;
 #[serde(rename_all = "camelCase")]
 pub struct CourseRevision {
     title: String,
-    content: String,
+    description: String,
     meta_description: String,
 }
 
 impl From<&AbstractEntityRevision> for CourseRevision {
     fn from(abstract_entity_revision: &AbstractEntityRevision) -> Self {
         let title = abstract_entity_revision.fields.get_or("title", "");
-        let content = abstract_entity_revision.fields.get_or("content", "");
+        let description = abstract_entity_revision.fields.get_or("description", "");
         let meta_description = abstract_entity_revision
             .fields
             .get_or("meta_description", "");
 
         CourseRevision {
             title,
-            content,
+            description,
             meta_description,
         }
     }

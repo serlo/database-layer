a37c5cecaf47563eb37f44f04bc4e9d3044e823f
Stephan Kulla <git.mail@kulla.me>
2021-10-28 19:52:07 +0200

    Refactor tests/entity.rs

--- server/tests/entity.rs
+++ server/tests/entity.rs
@@ -1,40 +1,17 @@
 #[cfg(test)]
 mod tests {
-    use actix_web::{test, App};
-    use std::str::from_utf8;
-
-    use server::{configure_app, create_database_pool};
+    use test_utils::*;
 
     #[actix_rt::test]
     async fn unrevised_entities_query() {
-        let pool = create_database_pool().await.unwrap();
-        let app = configure_app(App::new(), pool);
-        let app = test::init_service(app).await;
-        let req = test::TestRequest::post()
-            .uri("/")
-            .set_json(&serde_json::json!({
-                "type": "UnrevisedEntitiesQuery",
-                "payload": {}
-            }))
-            .to_request();
-        let resp = test::call_service(&app, req).await;
+        let response = Message::new("UnrevisedEntitiesQuery", json!({}))
+            .execute()
+            .await;
 
-        assert!(resp.status().is_success());
-
-        let result = json::parse(from_utf8(&test::read_body(resp).await).unwrap()).unwrap();
-
-        assert_eq!(
-            result,
-            json::object! {
-                "unrevisedEntityIds": [
-                    26892,
-                    33582,
-                    34741,
-                    34907,
-                    35247,
-                    35556
-                 ]
-            }
-        );
+        assert_ok(
+            response,
+            json!({ "unrevisedEntityIds": [26892, 33582, 34741, 34907, 35247, 35556] }),
+        )
+        .await;
     }
 }

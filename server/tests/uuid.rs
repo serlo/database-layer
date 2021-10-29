6c1b36364861bdb7e79fe3a0a209346b06aabfc0
Stephan Kulla <git.mail@kulla.me>
2021-10-29 11:28:53 +0200

    refactor server/tests/uuid.rs

--- server/tests/uuid.rs
+++ server/tests/uuid.rs
@@ -2,6 +2,7 @@
 mod tests {
     use actix_web::{test, App};
     use std::str::from_utf8;
+    use test_utils::*;
 
     use server::uuid::{uuid_query, UuidMessage};
     use server::{configure_app, create_database_pool};
@@ -27,28 +28,22 @@ mod tests {
 
     #[actix_rt::test]
     async fn exercise_group_property_cohesive() {
-        let pool = create_database_pool().await.unwrap();
+        let mut transaction = begin_transaction().await;
 
         let exercise_group_revision_id = 26070;
+        set_entity_revision_field(
+            exercise_group_revision_id,
+            "cohesive",
+            "true",
+            &mut transaction,
+        )
+        .await
+        .unwrap();
 
-        set_entity_revision_field(exercise_group_revision_id, "cohesive", "true", &pool)
-            .await
-            .unwrap();
+        let response = Message::new("UuidQuery", json!({ "id": exercise_group_revision_id }))
+            .execute()
+            .await;
 
-        let app = configure_app(App::new(), pool);
-        let app = test::init_service(app).await;
-        let message = UuidMessage::UuidQuery(uuid_query::Payload {
-            id: exercise_group_revision_id,
-        });
-        let req = test::TestRequest::post()
-            .uri("/")
-            .set_json(&message)
-            .to_request();
-        let resp = test::call_service(&app, req).await;
-
-        assert!(resp.status().is_success());
-
-        let uuid = json::parse(from_utf8(&test::read_body(resp).await).unwrap()).unwrap();
-        assert_eq!(uuid["cohesive"], true);
+        assert_ok_with(response, |result| assert_eq!(result["cohesive"], true)).await;
     }
 }

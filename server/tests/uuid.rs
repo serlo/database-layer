68449570c112b93973097b9d2610f6b388fbdcd1
Stephan Kulla <git.mail@kulla.me>
2021-10-29 11:39:25 +0200

    Refactor server/tests/uuid.rs

--- server/tests/uuid.rs
+++ server/tests/uuid.rs
@@ -1,29 +1,25 @@
 #[cfg(test)]
 mod tests {
     use actix_web::{test, App};
-    use std::str::from_utf8;
     use test_utils::*;
 
-    use server::uuid::{uuid_query, UuidMessage};
     use server::{configure_app, create_database_pool};
-    use test_utils::set_entity_revision_field;
 
     #[actix_rt::test]
     async fn test_pact1() {
         let pool = create_database_pool().await.unwrap();
         let app = configure_app(App::new(), pool);
         let app = test::init_service(app).await;
-        let message = UuidMessage::UuidQuery(uuid_query::Payload { id: 1 });
         let req = test::TestRequest::post()
             .uri("/")
-            .set_json(&message)
+            .set_json(&json!({ "type": "UuidQuery", "payload": { "id": 1 } }))
             .to_request();
-        let resp = test::call_service(&app, req).await;
+        let response = test::call_service(&app, req).await;
 
-        assert!(resp.status().is_success());
-
-        let uuid = json::parse(from_utf8(&test::read_body(resp).await).unwrap()).unwrap();
-        assert_eq!(uuid["__typename"], "User");
+        assert_ok_with(response.into(), |result| {
+            assert_eq!(result["__typename"], "User")
+        })
+        .await;
     }
 
     #[actix_rt::test]

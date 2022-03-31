de4da92f5d72161d072dea4fff3dd4e9bd6bcd6b
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-03-31 11:32:27 +0200

    chore(version-script): automatize writing of version

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,12 +2,23 @@
 
 source scripts/utils.sh
 
-nvim server/Cargo.toml
-cargo update -p server
-
 nvim scripts/changelog.ts
+
 print_header "Generating CHANGELOG"
 yarn ts-node scripts/changelog.ts
 
+print_header "Updating version in Cargo.toml"
+VERSION=$(cat scripts/changelog.ts \
+  | grep 'name:' \
+  | tail -1 \
+  | awk -F: '{ print $2 }' \
+  | sed "s/[', ]//g")
+
+sed -i "0,/version/{s/version.*$/version = \"$VERSION\"/g}" server/Cargo.toml
+cargo update -p server
+
+print_header "Formatting"
 yarn format
+
+print_header "Time to commit"
 git add -p

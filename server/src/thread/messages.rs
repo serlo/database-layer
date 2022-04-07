ef6691e63597cc8f6cccb3218b0fd51ce76167ce
Stephan Kulla <git.mail@kulla.me>
2022-04-07 10:35:57 +0200

    Use trait Operation for ThreadCreateCommentMutation

--- server/src/thread/messages.rs
+++ server/src/thread/messages.rs
@@ -16,7 +16,7 @@ use crate::message::MessageResponder;
 pub enum ThreadMessage {
     ThreadsQuery(threads_query::Payload),
     ThreadCreateThreadMutation(create_thread_mutation::Payload),
-    ThreadCreateCommentMutation(ThreadCreateCommentMutation),
+    ThreadCreateCommentMutation(create_comment_mutation::Payload),
     ThreadSetThreadArchivedMutation(ThreadSetThreadArchivedMutation),
 }
 
@@ -33,7 +33,11 @@ impl MessageResponder for ThreadMessage {
                     .handle("ThreadCreateCommentMutation", connection)
                     .await
             }
-            ThreadMessage::ThreadCreateCommentMutation(message) => message.handle(connection).await,
+            ThreadMessage::ThreadCreateCommentMutation(message) => {
+                message
+                    .handle("ThreadCreateCommentMutation", connection)
+                    .await
+            }
             ThreadMessage::ThreadSetThreadArchivedMutation(message) => {
                 message.handle(connection).await
             }
@@ -93,66 +97,53 @@ pub mod create_thread_mutation {
     }
 }
 
-#[derive(Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ThreadCreateCommentMutation {
-    pub thread_id: i32,
-    pub content: String,
-    pub user_id: i32,
-    pub subscribe: bool,
-    pub send_email: bool,
-}
+pub mod create_comment_mutation {
+    use super::*;
+    #[derive(Deserialize, Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Payload {
+        pub thread_id: i32,
+        pub content: String,
+        pub user_id: i32,
+        pub subscribe: bool,
+        pub send_email: bool,
+    }
 
-#[derive(Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ThreadCreateCommentMutationResponse {
-    pub success: bool,
-    pub reason: Option<String>,
-}
+    #[derive(Deserialize, Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct ThreadCreateCommentMutationResponse {
+        pub success: bool,
+        pub reason: Option<String>,
+    }
 
-#[async_trait]
-impl MessageResponder for ThreadCreateCommentMutation {
-    #[allow(clippy::async_yields_async)]
-    async fn handle(&self, connection: Connection<'_, '_>) -> HttpResponse {
-        let payload = ThreadCommentThreadPayload {
-            thread_id: self.thread_id,
-            content: self.content.clone(),
-            user_id: self.user_id,
-            subscribe: self.subscribe,
-            send_email: self.send_email,
-        };
-        let response = match connection {
-            Connection::Pool(pool) => Threads::comment_thread(payload, pool).await,
-            Connection::Transaction(transaction) => {
-                Threads::comment_thread(payload, transaction).await
-            }
-        };
-        match response {
-            Ok(data) => HttpResponse::Ok()
-                .content_type("application/json; charset=utf-8")
-                .json(&data),
-            Err(e) => {
-                println!("/thread/comment-thread: {:?}", e);
-                match e {
-                    ThreadCommentThreadError::DatabaseError { .. } => {
-                        HttpResponse::InternalServerError().finish()
-                    }
-                    ThreadCommentThreadError::ThreadArchivedError { .. } => {
-                        HttpResponse::InternalServerError().finish()
-                    }
-                    ThreadCommentThreadError::EventError { .. } => {
-                        HttpResponse::InternalServerError().finish()
-                    }
-                    ThreadCommentThreadError::UuidError { .. } => {
-                        HttpResponse::InternalServerError().finish()
-                    }
-                    ThreadCommentThreadError::BadUserInput { reason } => HttpResponse::BadRequest()
-                        .content_type("application/json; charset=utf-8")
-                        .json(ThreadCreateCommentMutationResponse {
-                            success: false,
-                            reason: Some(format!("Cannot create comment: {}", reason)),
-                        }),
+    #[async_trait]
+    impl Operation for Payload {
+        type Output = Uuid;
+
+        async fn execute(&self, connection: Connection<'_, '_>) -> operation::Result<Self::Output> {
+            let payload = ThreadCommentThreadPayload {
+                thread_id: self.thread_id,
+                content: self.content.clone(),
+                user_id: self.user_id,
+                subscribe: self.subscribe,
+                send_email: self.send_email,
+            };
+            Ok(match connection {
+                Connection::Pool(pool) => Threads::comment_thread(payload, pool).await?,
+                Connection::Transaction(transaction) => {
+                    Threads::comment_thread(payload, transaction).await?
                 }
+            })
+        }
+    }
+
+    impl From<ThreadCommentThreadError> for operation::Error {
+        fn from(e: ThreadCommentThreadError) -> Self {
+            match e {
+                ThreadCommentThreadError::BadUserInput { reason } => {
+                    operation::Error::BadRequest { reason }
+                }
+                _ => operation::Error::InternalServerError { error: Box::new(e) },
             }
         }
     }

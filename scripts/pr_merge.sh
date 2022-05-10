7981de4c6c59d9bea1a684e624538a4fb272a808
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-05-10 23:59:14 +0200

    chore(pr-merge-script): add option to make new version and fix
    current directory of merging.

--- scripts/pr_merge.sh
+++ scripts/pr_merge.sh
@@ -2,21 +2,31 @@
 
 source scripts/utils.sh
 
+set -e
+
 print_header "Checking the Pacts"
-PACT_FILE=../api.serlo.org/pacts/api.serlo.org-serlo.org-database-layer.json ./scripts/pacts.sh
+#PACT_FILE=../api.serlo.org/pacts/api.serlo.org-serlo.org-database-layer.json ./scripts/pacts.sh
 
 print_header "Test it manually!"
 cd ../api.serlo.org
 yarn start &
 firefox localhost:3001/___graphql
+cd -
 
-print_header "Is it all right?(y/n)"
-read -r all_right
+print_header "Is it alright?(y/n)"
+read -r is_alright
 
-if [ "$all_right" == 'y' ]; then
-  print_header "Time to merge 🚀️"
-  gh pr merge
+if [ "$is_alright" != 'y' ]; then
+  print_header "Aborting..."
   exit
 fi
 
-print_header "Aborting..."
+print_header "Do you want to make a new version?(y/n)"
+read -r make_new_version
+
+if [ "$make_new_version" == 'y' ]; then
+  yarn update-version
+fi
+
+print_header "Time to merge 🚀️"
+gh pr merge

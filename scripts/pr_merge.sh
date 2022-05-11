f065bdee7ec750c6bd247f0c08174aff97c016ff
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-05-11 09:53:55 +0200

    chore(pr-merge-script): remind to push the new version and minor fix

--- scripts/pr_merge.sh
+++ scripts/pr_merge.sh
@@ -5,13 +5,14 @@ source scripts/utils.sh
 set -e
 
 print_header "Checking the Pacts"
-#PACT_FILE=../api.serlo.org/pacts/api.serlo.org-serlo.org-database-layer.json ./scripts/pacts.sh
+PACT_FILE=../api.serlo.org/pacts/api.serlo.org-serlo.org-database-layer.json ./scripts/pacts.sh
 
 print_header "Test it manually!"
 cd ../api.serlo.org
 yarn start &
 firefox localhost:3001/___graphql
 cd -
+cargo run &
 
 print_header "Is it alright?(y/n)"
 read -r is_alright
@@ -26,6 +27,8 @@ read -r make_new_version
 
 if [ "$make_new_version" == 'y' ]; then
   yarn update-version
+  print_header "Push the commit and wait the checks have passed"
+  print_header "Aborting PR merging..."
 fi
 
 print_header "Time to merge 🚀️"

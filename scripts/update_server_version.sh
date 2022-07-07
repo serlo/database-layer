4a68e68bd3ecd81202aa24c9bbb16555cec5828c
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-07-07 14:49:31 +0200

    chore(versioning): add simple script for generating change log entry and
    minor change.

--- scripts/update_server_version.sh
+++ scripts/update_server_version.sh
@@ -4,6 +4,8 @@ set -e
 
 source scripts/utils.sh
 
+git stash --include-untracked
+
 $(git config core.editor) scripts/changelog.ts
 
 print_header "Generating CHANGELOG"
@@ -25,3 +27,5 @@ yarn format
 print_header "Time to commit 🚀️"
 git add -p
 git commit
+
+git stash pop

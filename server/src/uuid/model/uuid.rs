be1b51d7399c7eabbda72c1d6bbbb37c7faea1a2
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-12-28 21:01:22 +0100

    fix(set-uuid-state): exclude entity revisions from query
    since they are anyway not trashable.

--- server/src/uuid/model/uuid.rs
+++ server/src/uuid/model/uuid.rs
@@ -277,14 +277,12 @@ impl Uuid {
                         UNION ALL
                         SELECT id, instance_id FROM entity
                         UNION ALL
-                        SELECT er.id, e.instance_id FROM entity_revision er JOIN entity e ON er.repository_id = e.id
-                        UNION ALL
                         SELECT id, instance_id FROM page_repository
                         UNION ALL
                         SELECT pr.id, p.instance_id FROM page_revision pr JOIN page_repository p ON pr.page_repository_id = p.id
                         UNION ALL
-                        SELECT term_taxonomy.id, instance_id FROM term_taxonomy
-                        JOIN term ON term.id = term_taxonomy.term_id) c ON c.id = u.id
+                        SELECT term_taxonomy.id, instance_id FROM term_taxonomy JOIN term ON term.id = term_taxonomy.term_id
+                        ) c ON c.id = u.id
                         JOIN instance i ON i.id = c.instance_id
                         WHERE u.id = ?
                 "#,
@@ -292,9 +290,10 @@ impl Uuid {
             )
             .fetch_one(&mut transaction)
             .await;
-
             match result {
                 Ok(uuid) => {
+                    // Actually the query already exludes entity revisions and users.
+                    // But we can leave it as kind of reminder in case the query is wrongly refactored.
                     if uuid.discriminator == "entityRevision" || uuid.discriminator == "user" {
                         return Err(operation::Error::BadRequest {
                             reason: format!(

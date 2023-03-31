ff4d82e40d5286072e3ea059363aaf091fb0984f
Mikey Stengel <mikey@serlo.org>
2023-03-31 10:49:17 +0200

    Add tests

--- server/tests/metadata.rs
+++ server/tests/metadata.rs
@@ -103,4 +103,128 @@ mod entities_metadata_query {
             .await
             .should_be_bad_request();
     }
+
+    #[actix_rt::test]
+    async fn checks_mandatory_keys_in_response() {
+        Message::new("EntitiesMetadataQuery", json!({ "first": 350 }))
+            .execute()
+            .await
+            .should_be_ok_with(|value| {
+                let mandatory_keys = [
+                    "@context",
+                    "id",
+                    "type",
+                    "name",
+                    "publisher",
+                    "learningResourceType",
+                ];
+
+                let entities = value["entities"].as_array().unwrap();
+                for entity in entities {
+                    for mandatory_key in &mandatory_keys {
+                        assert!(
+                            entity.get(mandatory_key).is_some(),
+                            "Mandatory key '{}' is missing from the response",
+                            mandatory_key
+                        );
+                    }
+                }
+            });
+    }
+
+    #[actix_rt::test]
+    async fn checks_mandatory_keys_in_response_of_a_quiz() {
+        // Fetching a quiz https://serlo.org/20256
+        Message::new(
+            "EntitiesMetadataQuery",
+            json!({ "first": 1, "after": 20_000  }),
+        )
+        .execute()
+        .await
+        .should_be_ok_with(|value| {
+            assert_eq!(
+                value["entities"][0]["name"],
+                "Quiz: https://serlo.org/20256"
+            );
+            let mandatory_keys = [
+                "@context",
+                "id",
+                "type",
+                "name",
+                "publisher",
+                "learningResourceType",
+            ];
+
+            let entities = value["entities"].as_array().unwrap();
+            for entity in entities {
+                for mandatory_key in &mandatory_keys {
+                    assert!(
+                        entity.get(mandatory_key).is_some(),
+                        "Mandatory key '{}' is missing from the Quiz!",
+                        mandatory_key
+                    );
+                }
+            }
+        });
+    }
+
+    #[actix_rt::test]
+    async fn checks_optional_keys_in_response() {
+        Message::new("EntitiesMetadataQuery", json!({ "first": 300, }))
+            .execute()
+            .await
+            .should_be_ok_with(|value| {
+                let optional_keys = [
+                    "description",
+                    "about",
+                    "keywords",
+                    "inLanguage",
+                    "image",
+                    "trailer",
+                    "creator",
+                    "contributor",
+                    "affiliation",
+                    "dateCreated",
+                    "datePublished",
+                    "dateModified",
+                    "isAccessibleForFree",
+                    "license",
+                    "conditionsOfAccess",
+                    "audience",
+                    "teaches",
+                    "assesses",
+                    "competencyRequired",
+                    "educationalLevel",
+                    "interactivityType",
+                    "isBasedOn",
+                    "isPartOf",
+                    "hasPart",
+                    "mainEntityOfPage",
+                    "duration",
+                    "encoding",
+                    "caption",
+                ];
+
+                let entities = value["entities"].as_array().unwrap();
+                for entity in entities {
+                    for optional_key in &optional_keys {
+                        if let Some(field_value) = entity.get(optional_key) {
+                            // TODO add more specific checks for each optional
+                            // field's value and their respective schemas
+                            match *optional_key {
+                                "isAccessibleForFree" => {
+                                    assert!(
+                                        field_value.as_bool() == Some(true),
+                                        "Invalid value for optional key '{}'.
+                                        We only offer freely accessible learning resources!",
+                                        optional_key
+                                    );
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+            });
+    }
 }

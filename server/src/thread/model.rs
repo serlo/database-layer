7bfb93a0ffb64f7e41f7d777fbc04213457eb558
MoeHome <moritz@heimbaecher.eu>
2023-06-02 16:12:12 +0200

    altered query to filter by subject_id
    Co-authored-by: eliflores <eli@serlo.org>

--- server/src/thread/model.rs
+++ server/src/thread/model.rs
@@ -48,15 +48,26 @@ impl Threads {
         let result = sqlx::query!(
             r#"
                 WITH RECURSIVE descendants AS (
-                  SELECT id, parent_id
-                  FROM term_taxonomy
-                  WHERE (? is null OR id = ?)
+                    SELECT id, parent_id
+                    FROM term_taxonomy
+                    WHERE (? is null OR id = ?)
 
-                  UNION ALL
+                    UNION
 
-                  SELECT tt.id, tt.parent_id
-                  FROM term_taxonomy tt
-                  INNER JOIN descendants d ON tt.parent_id = d.id
+                    SELECT tt.id, tt.parent_id
+                    FROM term_taxonomy tt
+                    JOIN descendants d ON tt.parent_id = d.id
+                ), subject_entities AS (
+                SELECT tte.entity_id
+                FROM descendants
+                JOIN term_taxonomy_entity tte ON descendants.id = tte.term_taxonomy_id
+
+                UNION
+
+                SELECT entity_link.child_id
+                FROM descendants
+                JOIN term_taxonomy_entity tte ON descendants.id = tte.term_taxonomy_id
+                JOIN entity_link ON entity_link.parent_id = tte.entity_id
                 )
                 SELECT comment.id
                 FROM comment
@@ -64,7 +75,7 @@ impl Threads {
                 JOIN comment answer ON comment.id = answer.parent_id OR
                     comment.id = answer.id
                 JOIN uuid parent_uuid ON parent_uuid.id = comment.uuid_id
-                JOIN descendants ON descendants.id = comment.uuid_id
+                JOIN subject_entities ON subject_entities.entity_id = comment.uuid_id
                 WHERE
                     comment.uuid_id IS NOT NULL
                     AND uuid.trashed = 0
@@ -74,7 +85,7 @@ impl Threads {
                 GROUP BY comment.id
                 HAVING MAX(GREATEST(answer.date, comment.date)) < ?
                 ORDER BY MAX(GREATEST(answer.date, comment.date)) DESC
-                LIMIT ?
+                LIMIT ?;
             "#,
             subject_id,
             subject_id,

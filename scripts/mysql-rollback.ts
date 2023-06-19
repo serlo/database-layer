775edbaef7d06e602dfe64a05d353e52c816d7fc
Stephan Kulla <git.mail@kulla.me>
2023-06-19 22:00:16 +0200

    chore(mysql): Refactor scripts/mysql-rollback.ts

--- scripts/mysql-rollback.ts
+++ scripts/mysql-rollback.ts
@@ -1,21 +1,8 @@
-import { spawn } from 'node:child_process'
-import * as process from 'node:process'
+import { spawnSync } from 'node:child_process'
 
 const mysqlRollbackCmd =
-  'mysql serlo < /docker-entrypoint-initdb.d/001-init.sql'
+  'pv /docker-entrypoint-initdb.d/001-init.sql | mysql serlo'
+const args = ['exec', 'mysql', 'sh', '-c', mysqlRollbackCmd]
+const opt = { stdio: [process.stdin, process.stdout, process.stderr] }
 
-const dockerComposeArgs = ['exec', '-T', 'mysql', 'sh', '-c', mysqlRollbackCmd]
-
-const sqlRollback = spawn('docker-compose', dockerComposeArgs)
-
-sqlRollback.stdout.pipe(process.stdout)
-
-sqlRollback.stderr.pipe(process.stderr)
-
-sqlRollback.on('error', (error) => {
-  console.error('ERROR: ' + error)
-})
-
-sqlRollback.on('exit', (code) => {
-  process.exit(code !== null ? code : 1)
-})
+spawnSync('docker-compose', args, opt)

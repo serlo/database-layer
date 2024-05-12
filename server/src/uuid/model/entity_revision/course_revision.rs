72c84625ba605a82a70c5688359d352c54e825c9
Stephan Kulla <git.mail@kulla.me>
2024-05-12 14:05:16 +0200

    fix(course): Let courses return their content

--- server/src/uuid/model/entity_revision/course_revision.rs
+++ server/src/uuid/model/entity_revision/course_revision.rs
@@ -13,7 +13,7 @@ pub struct CourseRevision {
 impl From<&AbstractEntityRevision> for CourseRevision {
     fn from(abstract_entity_revision: &AbstractEntityRevision) -> Self {
         let title = abstract_entity_revision.fields.get_or("title", "");
-        let content = abstract_entity_revision.fields.get_or("description", "");
+        let content = abstract_entity_revision.fields.get_or("content", "");
         let meta_description = abstract_entity_revision
             .fields
             .get_or("meta_description", "");
